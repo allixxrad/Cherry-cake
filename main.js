31834e40dc45bc32eee64ceed5caa0e347359751
Alina Ruban <alinaruban0903@gmail.com>
2024-09-14 16:10:42 +0300

    cherry cake

---
+++ main.js
@@ -0,0 +1,7 @@
+
+script
+document.querySelector('.burger').addEventListener('click', function() {
+    this.classList.toggel('active');
+    document.querySelector('.nav').classList.toggle('open')
+})
+script
